058872b249fa6219b7d800266ca07ffcfd32763f
enesozmus <ensozmus@gmail.com>
2022-11-05 11:27:12 +0300

    auto-login and auto-logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -11,16 +11,16 @@ import { User } from '../models/user.model';
 })
 export class AuthService {
 
-/** BehaviorSubject
- * Compare it with the subscription to a newspaper: 
- *  → After subscribing at a certain point in time X, the first edition you will get will be the one published after X.
- *  → This is how a normal 'Subject' works.
- * 
- * ♫ If the subscription works like the one to a BehaviorSubject,
- *    → you would get the latest edition that was published before X, instantly at the moment you are subscribing.
- * 
- * . BehaviorSubject needs to be initialized with a starting value.
- */
+  /** BehaviorSubject
+   * Compare it with the subscription to a newspaper: 
+   *  → After subscribing at a certain point in time X, the first edition you will get will be the one published after X.
+   *  → This is how a normal 'Subject' works.
+   * 
+   * ♫ If the subscription works like the one to a BehaviorSubject,
+   *    → you would get the latest edition that was published before X, instantly at the moment you are subscribing.
+   * 
+   * . BehaviorSubject needs to be initialized with a starting value.
+   */
 
 
 
@@ -28,7 +28,7 @@ export class AuthService {
   // This will inform all places in the application about when authenticated user changes.
   // We can subscribe and we'll get information whenever new data is emitted.
   // → user = new Subject<User>();
-  
+
   // We can call next, to emit a value and we can subscribe to it to be informed about new values.
   // It also gives subscribers immediate access to the previously emitted value
   // even if they haven't subscribed at the point of time that value was emitted.
@@ -40,6 +40,7 @@ export class AuthService {
    *  we can get access to that latest user.
    */
   user = new BehaviorSubject<User>(null);
+  private tokenExpirationTimer: any;
 
   constructor(private http: HttpClient, private router: Router) { }
 
@@ -82,19 +83,55 @@ export class AuthService {
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
+    localStorage.removeItem('userData');
+    //localStorage.clear();
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
+  }
+
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+      this.autoLogout(expirationDuration);
+    }
+  }
+
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);
   }
 
   private handleError(httpErrorResponse: HttpErrorResponse) {
 
     let errorMessage = 'An unknown error occured!';
 
-    if (!httpErrorResponse.error || !httpErrorResponse.error.error)
-    {
+    if (!httpErrorResponse.error || !httpErrorResponse.error.error) {
       return throwError(() => new Error(errorMessage));
     }
 
-    switch (httpErrorResponse.error.error.message)
-    {
+    switch (httpErrorResponse.error.error.message) {
       case 'EMAIL_EXISTS':
         errorMessage = 'The email address is already in use by another account.'
         break;
@@ -119,10 +156,11 @@ export class AuthService {
     return throwError(() => new Error(errorMessage));
   }
 
-  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number)
-  {
+  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    this.autoLogout(expiresIn * 1000);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 }
