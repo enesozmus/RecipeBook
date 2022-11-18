b20d872c0da55dbddd7ca53196e4ae189efd267a
enesozmus <ensozmus@gmail.com>
2022-11-18 19:22:41 +0300

    Adding Logic to the Reducer

--- src/app/reducers/shopping-list.reducer.ts
+++ src/app/reducers/shopping-list.reducer.ts
@@ -9,9 +9,17 @@
 /**
  * Let's get started by implementing a reducer.
  * Here, we should export a function and define an initialState.
+ *    → Each reducer function is a listener of actions.
+ *    → The reducer function's responsibility is to handle the state transitions in an immutable way.
+ */
+
+/**
+ * You define the shape of the state according to what you are capturing,
+ * ...whether it be a single type such as a number, or a more complex object with multiple properties.
  */
 
 import { Ingredient } from "../models/ingredient.model";
+import { Action } from "@ngrx/store";
 
 /**
  * The initial state gives the state an initial value, or provides a value if the current state is undefined.
@@ -32,6 +40,26 @@ export const initialState = {
 
 // Now we can set that initialState by giving this first argument in that function.
 
-export function shoppingListReducer(state = initialState, action) {
+export function shoppingListReducer(state = initialState, action: Action) {
 
+  /**
+   * Now, to check different kinds of possible actions, you could add multiple if statements and run different codes depending on which action you have.
+   * Or since you have multiple possible conditions, you can use a switch case statement.
+   */
+   switch (action.type) {
+    case 'ADD_INGREDIENT':
+      /**
+       * So never touch the existing state.
+       * Instead return a new object which will replace the old state
+       * and make sure you haven't lost all the old data.
+       * copy the old state with the spread operator (...)
+       *  → It essentially pulls out all the properties of the old state and adds these properties to this new object.
+       *  → We have a new object with the old data and hence we have a copy of the old state.
+       *  → return {...state}
+       */
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action]
+      };
+  }
 }
